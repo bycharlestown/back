75e5547d81cf244e9b4c09c061ba716c0d7fa25f
Artur <tullers@mail.ru>
2021-12-30 13:45:50 +0300

    get card ID

--- controller/parser.controller.js
+++ controller/parser.controller.js
@@ -3,20 +3,32 @@ import client from "../db.js";
 client.connect();
 
 class ParserController {
-  // CHECKING DATA BASE INFO
-  async getAllParsingInfo(req, res) {
+  // Get all CARDS
+  getCards(req, res) {
+    client.query("SELECT * FROM cards_info;", (err, resp) => {
+      if (err) throw err;
+
+      const result = resp.rows;
+
+      res.send(result);
+    });
+  }
+
+  // Get description of the CARD ID
+  getFullDescriptions(req, res) {
+    const { id } = req.params;
+
     client.query(
-      "SELECT * FROM cards_info LEFT JOIN full_descriptions ON cards_info.id = full_descriptions.card_id ",
-      async (err, resp) => {
+      "SELECT * FROM full_descriptions WHERE card_id = $1;",
+      [id],
+      (err, resp) => {
         if (err) throw err;
 
-        const result = resp.rows;
+        const result = resp.rows[0];
 
         res.send(result);
       }
     );
-
-    // client.end();
   }
 }
 
--- routes/parser.route.js
+++ routes/parser.route.js
@@ -3,6 +3,7 @@ import ParserController from "../controller/parser.controller.js";
 
 const router = new Router();
 
-router.get("/parsing_info", ParserController.getAllParsingInfo);
+router.get("/parsing_info", ParserController.getCards);
+router.get("/parsing_info/:id", ParserController.getFullDescriptions);
 
 export default router;
